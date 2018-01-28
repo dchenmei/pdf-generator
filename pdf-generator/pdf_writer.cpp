7c1b1ce1197710bb08aa6eed624b44a941b25437
swolewizard <swolewizard@users.noreply.github.com>
2018-01-27 21:21:51 -0500

    Added a little more structure to pdf_writer

--- pdf-generator/pdf_writer.cpp
+++ pdf-generator/pdf_writer.cpp
@@ -1,9 +1,52 @@
 #include "pdf_writer.h"
 
-PDFWriter::PDFWriter(FILE *f, string e): file(f), extension(e)
+// Public 
+//
+PDFWriter::PDFWriter(FILE *f, string e): file(f), extension(e), out(e)
 {
+	// TODO: Get the original name instead?
 }
 
 PDFWriter::PDFWriter(FILE *f, string e, string o): file(f), extension(e), out(o)
 {
+}
+
+FILE* PDFWriter::write_to_pdf()
+{
+	FILE *pdf;
+	switch(extension[0])
+	{
+		case 't':
+			pdf = text_to_pdf();
+			break;
+		case 'j':
+			pdf = img_to_pdf();
+			break;
+		case 'h':
+			pdf = html_to_pdf();
+			break;
+		default:
+			break;
+	}
+
+	return pdf;
+}
+
+// Private
+
+FILE* PDFWriter::text_to_pdf()
+{
+}
+
+FILE* PDFWriter::img_to_pdf()
+{
+}
+
+FILE* PDFWriter::html_to_pdf()
+{
+}
+
+int main()
+{
+	return 0;
 }
--- pdf-generator/pdf_writer.h
+++ pdf-generator/pdf_writer.h
@@ -5,6 +5,7 @@
  *********************************************/
 
 #include <string>
+#define PDF_VERSION 1.7
 
 using namespace std;
 
@@ -13,11 +14,15 @@ class PDFWriter
   public:
 	PDFWriter(FILE *f, string e);           // write w/o output name 
 	PDFWriter(FILE *F, string e, string o); // writer w/ output name
+	FILE* write_to_pdf();
 
   private:
-	double version = 1.7;
+	// helper functions
+	FILE* text_to_pdf();
+	FILE* img_to_pdf();
+	FILE* html_to_pdf();
+
 	FILE *file;
 	string extension;
 	string out;
-	
 };
