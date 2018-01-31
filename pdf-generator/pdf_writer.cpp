a869fe7e00005b3f6f5664fc67a071dd029c1c10
swolewizard <swolewizard@users.noreply.github.com>
2018-01-30 19:30:55 -0500

    Outlined a new strategy for taking away way too many fputs

--- pdf-generator/pdf_writer.cpp
+++ pdf-generator/pdf_writer.cpp
@@ -36,12 +36,32 @@ FILE* PDFWriter::write_to_pdf()
 
 FILE* PDFWriter::text_to_pdf()
 {
+	// Option 1: Open one, copy to another (worse than writing one by one)
+	// Option 2: Have it stored, use a stream to dump all of it in (no need to clutter code with fputs)
+	/* Alternative solution, modifying a pdf template to save time */
+
+	/* Line to modify I: version number */
+
+
+	/* Line to modify II: length of stream */
+
+
+	/* Line(s) to modify III: Actual content */
+
+
+	/* Line(s) to modify IV: xref table */
+
+	
+	/* Line to modify V: final total byte value */
+	
+	#if 0
 	/* No need to check, a new file is created anyway */
 	string filename = out + ".pdf";
 	const char* f = filename.c_str();
 	FILE *pdf = fopen(f , "w");
 
 	/* Header */
+	// TODO: use the macro instead to keep it easier to update?
 	fputs("%PDF-1.7\n", pdf);
 
 	/* Necessary set up objects */
@@ -51,27 +71,44 @@ FILE* PDFWriter::text_to_pdf()
 	fputs("endobj\n", pdf);
 
 	/* Page obj */
-	fputs("2 0 obj <</Type /Pages /Kids [3 0 R]  /Count 1>>\n", pdf);
+	fputs("2 0 obj <</Type /Pages /Kids [3 0 R] /Count 1>>\n", pdf);
 	fputs("endobj\n", pdf);
 
 	/* Mediabox obj */
-	fputs("3 0 obj <</Mediabox [0 0 612 792]>>\n", pdf);
+	fputs("3 0 obj <</Type /Page /Parent 2 0 R /Resources 4 0 R /MediaBox [0 0 612 792] /Contents 6 0 R>>\n", pdf);
 	fputs("endobj\n", pdf);
 	
+	/* Font */
+	fputs("4 0 obj <</Font <</F1 5 0 R>>>>\n", pdf);
+	fputs("endobj\n", pdf);
+ 
+	fputs("5 0 obj <</Type /Font /Subtype /Type1 /BaseFont /Times-Roman>>\n", pdf);
+	fputs("endobj\n", pdf);
+
 	/* Content */
+	fputs("6 0 obj\n", pdf);
+	fputs("<</Length 44>>\n", pdf);
+	fputs("stream\n", pdf);
+	fputs("BT /F1 24 Tf 175 720 Td (Hello World!)Tj ET\n", pdf);
+	fputs("endstream\n", pdf);
+	fputs("endobj\n", pdf);
 
 	/* xref table */
 	fputs("xref\n", pdf);
-	fputs("0 4\n", pdf);
+	fputs("0 7\n", pdf);
 	fputs("0000000000 65535 f\n", pdf);
-	fputs("0000000010 00000 n\n", pdf);
-	fputs("0000000060 00000 n\n", pdf);
-	fputs("0000000115 00000 n\n", pdf);
-	fputs("trailer <</Size 4/Root 1 0 R>>\n", pdf);
+	fputs("0000000009 00000 n\n", pdf);
+	fputs("0000000056 00000 n\n", pdf);
+	fputs("0000000111 00000 n\n", pdf);
+	fputs("0000000212 00000 n\n", pdf);
+	fputs("0000000250 00000 n\n", pdf);
+	fputs("0000000317 00000 n\n", pdf);
+	fputs("trailer <</Size 7/Root 1 0 R>>\n", pdf);
 	fputs("startxref\n", pdf);
-	fputs("199\n", pdf);
+	fputs("406\n", pdf);
 	fputs("%%EOF\n", pdf);
 	fclose(pdf);
+	#endif
 }
 
 FILE* PDFWriter::img_to_pdf()
@@ -81,12 +118,3 @@ FILE* PDFWriter::img_to_pdf()
 FILE* PDFWriter::html_to_pdf()
 {
 }
-
-/* Main function for independent testing
- * TODO: Delete after completing project
- 
-int main()
-{
-	return 0;
-}
- */
